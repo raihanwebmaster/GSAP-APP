e3a6c01a8ed647dd1031c58fd666dc0e8922f4d9
raihanwebmaster <raihanemon2015@gmail.com>
2024-05-18 11:21:55 +0200

    implement Highlights section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">iPhone 15 Pro</p>
         <div className="md:w-10/12 w-9/12">
-          <video className="pointer-events-none" autoPlay muted playsInline={true} loop  key={videoSrc}>
+          <video className="pointer-events-none" autoPlay muted playsInline={true}  key={videoSrc}>
               <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
